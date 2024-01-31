30006cafb84976812d8c2d0b6015e83c70199ffc
Bug On <losh11o08o2001@gmail.com>
2024-02-01 01:30:23 +0530

    learned about variables, comments, and console

---
+++ index.js
@@ -0,0 +1,27 @@
+//Prints the message
+console.log("Hello World");
+//Variables
+
+//var, let, const
+
+var name="Santosh";
+//var can be override
+name="Khatri";
+//always use camelCase for joining two or more words for variables
+console.log(name);
+
+//let, const (ES6)
+let nameIs="Santosh";
+nameIs="Khatri";
+console.log(nameIs);
+
+const dada="Santosh";
+console.log(dada);
+
+//Ex--> Create 4 variables using const keyword for table, fan, chair, pen
+
+const table="redTable";
+const fan="whiteFan";
+const chair="blackChair";
+const pen="bluePen";
+console.log(table, fan, chair, pen);
